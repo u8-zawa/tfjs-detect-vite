23f91fb448fc5e5ee57ea3aa69d3c08a6a800e9f
u8-zawa <yuya0713.a@gmail.com>
2025-08-15 15:37:40 +0900

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    build: {
-        // 古いブラウザのサポートを無効にし、バンドルサイズを削減
-        target: 'esnext',
-        // ソースマップを生成しない
-        sourcemap: false,
-        // コードの圧縮を有効化
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                // 本番ビルドからconsole.logを削除
-                drop_console: true,
-                drop_debugger: true,
+export default defineConfig(({ command }) => {
+    return {
+        base: command === 'build' ? '/tfjs-detect-vite/' : './',
+        build: {
+            target: 'esnext',
+            sourcemap: false,
+            minify: 'terser',
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                },
             },
         },
-    },
-    server: {
-        // HTTPSを有効にすると、特にモバイルデバイスでのカメラアクセスが安定します
-        // https: true, // 必要に応じてコメントを解除し、`vite-plugin-mkcert` などを追加してください
+        server: {
+            // https: true,
+        }
     }
 });
